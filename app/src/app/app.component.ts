a1f37444bbbd8925104ee6794b515de5d73191e7
OrlandoPa <opadillar1@upao.edu.pe>
2024-10-05 03:10:51 -0500

    v1

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {IniciosesionComponent} from "./iniciosesion/iniciosesion.component";
+import {PiepaginaComponent} from "./piepagina/piepagina.component";
+import {NavegadorComponent} from "./navegador/navegador.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IniciosesionComponent, PiepaginaComponent, NavegadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ app/src/app/iniciosesion/iniciosesion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iniciosesion',
+  standalone: true,
+  imports: [],
+  templateUrl: './iniciosesion.component.html',
+  styleUrl: './iniciosesion.component.scss'
+})
+export class IniciosesionComponent {
+
+}
