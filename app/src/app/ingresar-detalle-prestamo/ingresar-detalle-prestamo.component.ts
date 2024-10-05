b8c7921df729148b5fa0ce75db8937e3e44ba6a6
Dylan Acevedo Villena <dylan.acevedo2843@gmail.com>
2024-10-05 18:54:36 -0500

    Validacion de numero entero y positivo

--- app/src/app/ingresar-detalle-prestamo/ingresar-detalle-prestamo.component.ts
+++ app/src/app/ingresar-detalle-prestamo/ingresar-detalle-prestamo.component.ts
@@ -5,8 +5,28 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './ingresar-detalle-prestamo.component.html',
-  styleUrl: './ingresar-detalle-prestamo.component.scss'
+  styleUrls: ['./ingresar-detalle-prestamo.component.scss']
 })
 export class IngresarDetallePrestamoComponent {
 
+  // Método para validar que solo se ingresen números
+  validateInput(event: KeyboardEvent) {
+    const keyCode = event.keyCode ? event.keyCode : event.which;
+
+    // Solo permitir números (0-9)
+    if (keyCode < 48 || keyCode > 57) {
+      event.preventDefault();
+    }
+  }
+
+  // Método para validar que el número ingresado sea positivo
+  validatePositive(event: any) {
+    const input = event.target.value;
+
+    // Si el número es negativo, se borra el valor y se muestra alerta
+    if (parseFloat(input) < 0) {
+      event.target.value = '';
+      alert('Por favor, ingrese un número positivo.');
+    }
+  }
 }
