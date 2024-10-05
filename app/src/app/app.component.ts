d5df0ec5e164b80167f0132fe98d57a5c8616e06
Dylan Acevedo Villena <dylan.acevedo2843@gmail.com>
2024-10-05 08:44:53 -0500

    Vista: Seleccionar tipo de préstamo

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SeleccionarTipoPrestamoComponent } from './seleccionar-tipo-prestamo/seleccionar-tipo-prestamo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SeleccionarTipoPrestamoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ app/src/app/seleccionar-tipo-prestamo/seleccionar-tipo-prestamo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seleccionar-tipo-prestamo',
+  standalone: true,
+  imports: [],
+  templateUrl: './seleccionar-tipo-prestamo.component.html',
+  styleUrl: './seleccionar-tipo-prestamo.component.scss'
+})
+export class SeleccionarTipoPrestamoComponent {
+
+}
