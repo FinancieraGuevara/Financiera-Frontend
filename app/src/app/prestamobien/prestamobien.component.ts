224ce6936c3363d1d84ccef8ecdb4291dcfd5a23
Dylan Acevedo Villena <dylan.acevedo2843@gmail.com>
2024-10-06 14:13:34 -0500

    Vista prestamo exitoso

---
+++ app/src/app/prestamobien/prestamobien.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prestamobien',
+  standalone: true,
+  imports: [],
+  templateUrl: './prestamobien.component.html',
+  styleUrl: './prestamobien.component.scss'
+})
+export class PrestamobienComponent {
+
+}
