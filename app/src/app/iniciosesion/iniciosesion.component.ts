1f97993168324ed520f7931db8bce126095535a9
Javier Aguilar <jaguilarb3@upao.edu.pe>
2024-10-06 14:58:45 -0500

    feat:Agregando alerta al login

--- app/src/app/iniciosesion/iniciosesion.component.ts
+++ app/src/app/iniciosesion/iniciosesion.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 import { CommonModule } from '@angular/common';
-import {Router, RouterLink} from '@angular/router';
-import {UserService} from "../../Servicios/Usuario/user.service";
-import {NavegadorComponent} from "../navegador/navegador.component";
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from "../../Servicios/Usuario/user.service";
+import { NavegadorComponent } from "../navegador/navegador.component";
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'; // Importación de NgBootstrap Modal (si estás usando NgBootstrap)
 
 @Component({
   selector: 'app-iniciosesion',
@@ -14,8 +15,13 @@ import {NavegadorComponent} from "../navegador/navegador.component";
 })
 export class IniciosesionComponent {
   formUser: FormGroup;
+  showError: boolean = false;
 
-  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) {
+  constructor(
+    private fb: FormBuilder, 
+    private userService: UserService, 
+    private router: Router, 
+  ) {
     this.formUser = this.fb.group({
       username: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4)]]
@@ -33,12 +39,16 @@ export class IniciosesionComponent {
         this.router.navigate(['/private/consulta']); // Redirigir después de iniciar sesión
       },
       error: (error) => {
+        
         console.error('Error en el inicio de sesión:', error);
+        
         // Maneja el error, por ejemplo, mostrando un mensaje al usuario
+          
         if (error.status === 302) {
           // Manejo de redirección si es necesario
           this.router.navigate(['/login']); // O la ruta deseada
-        }
+        }  
+        this.showError = true;   
       }
     });
   }
