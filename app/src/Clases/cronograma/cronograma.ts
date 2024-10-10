dc7beda98f0358c2fa84ef6d843a44f822d85e80
Javier Aguilar <jaguilarb3@upao.edu.pe>
2024-10-10 00:58:44 -0500

    feat:Agregando campos al cronograma para facilitar su lectura

--- app/src/Clases/cronograma/cronograma.ts
+++ app/src/Clases/cronograma/cronograma.ts
@@ -1,7 +1,9 @@
 export class Cronograma {
-
     id: number;
     nmrcuota: number;
     cuota: number;
+    interes:number;
+    capitalamortizado: number;
+    saldofinal: number;
     fechaPago: String;
 }
